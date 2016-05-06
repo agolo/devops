257caeb16ffad440bfc1d77ea25eaa3da2487390
oxymor0n <viet.tran@outlook.com>
2016-05-06 16:04:47 -0400

    Create install_elasticsearch_2.sh

---
+++ install_elasticsearch_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install java 8
+# NOTE: pwd must be the git repo directory for this to work correctly
+./install_java.sh
+
+# download the public key of the elasticsearch repository
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+# add the elasticsearch 2.x repository to the sources list
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+
+# update the package lists and install elasticsearch
+sudo apt-get update && sudo apt-get -y install elasticsearch
