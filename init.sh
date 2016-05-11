a0307b74a43362c60cfa16c8174f6d93b67ff61e
oxymor0n <oxymor0n@outlook.com>
2016-05-10 23:49:42 -0400

    add line to clone this repository

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# this script should be executed right after a VM is provisioned 
+# to set up the basic admin and devops tools 
+
 # install basic dev packages
-sudo apt-get install -y build-essential git wget htop multitail httpie tree
+sudo apt-get install -y build-essential git wget htop multitail httpie tree jq
+
+# clone the devops repository to get access to installation scripts
+git clone git@github.com:oxymor0n/devops.git /devops
