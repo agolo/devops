821fa27b95c499459795e9f73bef1818187fb948
oxymor0n <oxymor0n@outlook.com>
2016-05-10 23:17:40 -0400

    Create install_nginx.sh

---
+++ install_nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# download the public key to the nginx repository and add it to apt
+wget http://nginx.org/keys/nginx_signing.key
+sudo apt-key add nginx_signing.key
+
+# add the nginx repository to the sources list
+echo "deb http://nginx.org/packages/ubuntu/ trusty nginx" | sudo tee -a /etc/apt/sources.list.d/nginx.list
+echo "deb-src http://nginx.org/packages/ubuntu/ trusty nginx" | sudo tee -a /etc/apt/sources.list.d/nginx.list
+
+# update the package lists and install nginx
+sudo apt-get update && sudo apt-get install -y nginx
+
+# create sites-available and sites-enabled folders
+sudo mkdir /etc/nginx/sites-available
+sudo mkdir /etc/nginx/sites-enabled
+
+# include all files in sites-enabled in the nginx config
+echo "include /etc/nginx/sites-enabled/*;" | sudo tee -a /etc/nginx/conf.d/sites-enabled.conf
