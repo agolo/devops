b2c7bda7c36c38cc029be2e95949596f407cf4d0
oxymor0n <oxymor0n@outlook.com>
2016-05-11 00:56:37 -0400

    make devops writable for all users

--- init.sh
+++ init.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y build-essential git wget htop multitail httpie tree jq
 
 # clone the devops repository to get access to installation scripts
 git clone https://github.com/oxymor0n/devops.git /devops
+
+# make devops writable for all users
+chmod 777 /devops
